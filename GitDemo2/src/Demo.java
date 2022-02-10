532a1b92a5b5483486d64256982d1f3347214d4e
keerti-chaudhari <99351802+keerti-chaudhari@users.noreply.github.com>
2022-02-10 19:42:00 +0530

    Update Demo.java

--- GitDemo2/src/Demo.java
+++ GitDemo2/src/Demo.java
@@ -7,6 +7,13 @@ public class Demo {
 		System.out.println("Hello m1");
 	}
 	
+	public void m2()
+	{
+		
+		System.out.println("Hello m2");
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
