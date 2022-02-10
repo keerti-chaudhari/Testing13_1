58aa462e43bea309f754af3f5a54dc71feb9bfa3
Kirti <Kirti@DESKTOP-6HVRKEU>
2022-02-10 19:43:49 +0530

    By Kirti

--- GitDemo2/src/Demo.java
+++ GitDemo2/src/Demo.java
@@ -15,10 +15,12 @@ public class Demo {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		
 		System.out.println("Hello Main of GIT");
+		
+		Demo d=new Demo();
+		d.m1();
 	}
 
 }
